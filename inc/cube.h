b014311cb02e3b3ac671d2edaf09c085580d9229
ddzuba <ddzuba@student.42wolfsburg.de>
2023-03-28 22:26:53 +0200

    Start of the work, version pre-pre-pre alpha 0.1

---
+++ inc/cube.h
@@ -0,0 +1,178 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddzuba <ddzuba@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 21:20:04 by ddzuba            #+#    #+#             */
+/*   Updated: 2023/03/28 22:25:29 by ddzuba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE_H
+# define CUBE_H
+
+/******************************************************************************/
+/*								STANDART LIBS								  */
+/******************************************************************************/
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <limits.h>
+# include <math.h>
+# include "../src/libft/libft.h"
+# include "../src/mlx/mlx.h"
+# include "utils.h"
+# include "errors.h"
+
+/******************************************************************************/
+/*							    ENUM STRUCTS							  	  */
+/******************************************************************************/
+
+enum e_position
+{
+	X,
+	Y,
+	DIST,
+	COLOR,
+	POS_MAX
+};
+
+enum e_type
+{
+	HORIZONTAL,
+	VERTICAL,
+	RENDER_TEXT
+};
+
+enum e_color
+{
+	FLOOR,
+	CELLING
+};
+
+enum e_texture
+{
+	NORD,
+	SOUTH,
+	WEST,
+	EAST
+};
+
+/******************************************************************************/
+/*							       STRUCTS							  	 	  */
+/******************************************************************************/
+
+/* Player positioning struct. 
+** px - is position on x.
+** py - is position on y.
+** pdx - is player direction of view on x.
+** pdy - the same, but on view on y.
+** pa - is player angle, need for player FOV(Field Of View)*/
+typedef struct s_player
+{
+	float		px;
+	float		py;
+	float		pdx;
+	float		pdy;
+	float		pa;
+}		t_player;
+
+/* Image struct. Needs for manipulate image data, as read, write etc. */
+typedef struct s_image
+{
+	void		*img;
+	char		*address;
+	int			bits_per_pixel;
+	int			endian;
+	int			w_text;
+	int			h_text;
+	int			line_length;
+}		t_image;
+
+/*** r: A pointer to an array of floats representing the position of the ray.
+** hor: A pointer to an array of floats representing the point where the ray 
+** intersects a horizontal grid line.
+** ver: A pointer to an array of floats representing the point where the ray
+** intersects a vertical grid line.
+** dist: The distance between the player and the wall that the ray intersects.
+** line_o: The line offset of the current ray being cast.
+** line_h: The line height of the wall that the ray intersects.
+** color: The color of the wall that the ray intersects.
+** ra: The angle of the ray with respect to the player's view.
+** ca: The cosine of the angle of the ray with respect to the player's view.
+** texture: An array of pointers to t_img structs representing the textures
+** that can be applied to the walls. */
+typedef struct s_ray
+{
+	float		*r;
+	float		*hor;
+	float		*ver;
+	float		dist;
+	float		line_o;
+	float		line_h;
+	int			color;
+	float		ra;
+	float		ca;
+	t_image		*texture[3];
+}	t_ray;
+
+/* Engine struct for cube. Keeps all important struct inside */
+typedef struct s_cube
+{
+	void		*mlx;
+	void		*window;
+	t_image		image;
+	t_image		*tex[4];
+	t_player	player;
+	t_ray		*ray;
+}	t_cube;
+
+/* The main part of the project. Keeps all important information about
+programm. Description:
+
+**  *info: a pointer to an integer array that contains system information.
+**  fd: an integer file descriptor used for file I/O operations.
+**  *color: a pointer to an integer array that contains color information.
+**  **texture: a pointer to an array of strings representing texture data.
+**  *player: a pointer to an array of doubles containing player position
+and orientation data.
+**  height: the height of the game screen.
+**  width: the width of the game screen.
+**  map_s: a floating-point value representing the size of the game map.
+**  mini_s: the size of the minimap.
+**  **map: a pointer to an array of strings representing the game map.
+**  *int_map: a pointer to an integer array that represents the game map as
+a grid of values.
+**  *cube: a pointer to a t_cube struct containing graphics system data.
+**  key: a t_key struct containing keyboard input data.
+**  pos_ray[2]: an integer array representing the position of a ray in the
+game. */
+typedef struct s_system
+{
+	int			*info;
+	int			fd;
+	int			*color;
+	char		**texture;
+	double		*player;
+	size_t		height;
+	size_t		width;
+	float		map_s;
+	size_t		mini_s;
+	char		**map;
+	int			*int_map;
+	t_cube		*cube;
+	t_key		key;
+	int			pos_ray[2];
+}	t_system;
+
+/******************************************************************************/
+/*							   LIST OF FUNCTIONS							  */
+/******************************************************************************/
+
+#endif
---
+++ inc/errors.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddzuba <ddzuba@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 21:30:36 by ddzuba            #+#    #+#             */
+/*   Updated: 2023/03/28 21:42:57 by ddzuba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERRORS_H
+# define ERRORS_H
+
+/******************************************************************************/
+/*								ERRORS DEFINE								  */
+/******************************************************************************/
+
+# define ARG 			"This program requires only one argument"
+# define FORMAT 		"Invalid map format"
+# define ACCESS 		"Failed to open map"
+# define PATH_NO 		"Invalid path for north texture"
+# define PATH_SO 		"Invalid path for south texture"
+# define PATH_WE 		"Invalid path for west texture"
+# define PATH_EA 		"Invalid path for east texture"
+# define RGB_F 			"Floor color isn't in RGB format"
+# define RGB_C 			"Ceiling color isn't in RGB format"
+# define LOT_OF_SET 	"Too many settings"
+# define MISSING		"Missing setting informations"
+# define INVALID_SET 	"Invalid setting's line"
+# define INVALID_MAP 	"Invalid map"
+# define MALLOC 		"Malloc failed"
+
+#endif
